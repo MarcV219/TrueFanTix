1658b7ad0babc0a3ea3afdfcc1906ff18c6fb038
Marc Villeneuve <marc@ignitex.com>
2026-02-24 10:55:19 -0500

    Remove AppHeader and NavHistoryProvider from layout

--- truefantix-web/src/app/layout.tsx
+++ truefantix-web/src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import AppHeader from "./_components/appheader";
-import { NavHistoryProvider } from "./_components/navhistory";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,10 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <NavHistoryProvider>
-          <AppHeader />
-          {children}
-        </NavHistoryProvider>
+        {children}
       </body>
     </html>
   );
